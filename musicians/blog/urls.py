e70deeca228d13311ed9725bafc8fa8dd7d128c4
Pk13055 <Pk13055@users.noreply.github.com>
2022-01-11 14:46:32 +0400

    [Update] blog creation + author list view

--- musicians/blog/urls.py
+++ musicians/blog/urls.py
@@ -1,22 +1,51 @@
 from django.urls import path
 
-from .views import (BlogDeleteView, BlogListView, BlogUpdateView,
-                    EntryDeleteView, EntryDetailView, EntryListView,
-                    EntryUpdateView)
+from .views import (
+    BlogCreateView,
+    BlogDeleteView,
+    BlogListView,
+    BlogUpdateView,
+    EntryAuthorListView,
+    EntryCreateView,
+    EntryDeleteView,
+    EntryDetailView,
+    EntryListView,
+    EntryUpdateView,
+)
 
 
 app_name = "blog"
 
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='blog_list'),
-    path('edit/<int:pk>', BlogUpdateView.as_view(), name='blog_update'),
-    path('delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete'),
-    path('entries/<int:pk>', EntryListView.as_view(), name='entry_list'),
-    path('entries/<int:blog_id>/entry/<int:pk>',
-         EntryDetailView.as_view(), name='entry_details'),
-    path('entries/<int:blog_id>/entry/<int:pk>/delete',
-         EntryDeleteView.as_view(), name='entry_delete'),
-    path('entries/<int:blog_id>/entry/<int:pk>/edit',
-         EntryUpdateView.as_view(), name='entry_update'),
+    path("", BlogListView.as_view(), name="blog_list"),
+    path("new/", BlogCreateView.as_view(), name="blog_create"),
+    path("edit/<int:pk>", BlogUpdateView.as_view(), name="blog_update"),
+    path("delete/<int:pk>", BlogDeleteView.as_view(), name="blog_delete"),
+    path("entries/<int:pk>", EntryListView.as_view(), name="entry_list"),
+    path(
+        "entries/author/<int:author_id>",
+        EntryAuthorListView.as_view(),
+        name="entry_author_list",
+    ),
+    path(
+        "entries/<int:blog_id>/entry/new",
+        EntryCreateView.as_view(),
+        name="entry_create",
+    ),
+    path(
+        "entries/<int:blog_id>/entry/<int:pk>",
+        EntryDetailView.as_view(),
+        name="entry_details",
+    ),
+    path(
+        "entries/<int:blog_id>/entry/<int:pk>/delete",
+        EntryDeleteView.as_view(),
+        name="entry_delete",
+    ),
+    path(
+        "entries/<int:blog_id>/entry/<int:pk>/edit",
+        EntryUpdateView.as_view(),
+        name="entry_update",
+    ),
 ]
--- musicians/blog/views.py
+++ musicians/blog/views.py
@@ -1,50 +1,72 @@
 from django.urls import reverse
 from django.urls.base import reverse_lazy
 from django.views.generic import DetailView, ListView
-from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-from blog.models import Blog, Entry
+from blog.models import Author, Blog, Entry
 
 
 class BlogListView(ListView):
     """Displays all the blogs in a paginated fashion"""
 
     template_name = "blog/blog_list.html.j2"
-    context_object_name = 'blogs'
+    context_object_name = "blogs"
     paginate_by = 10
     model = Blog
+
+
+class BlogCreateView(CreateView):
+    """Form to create a new blog"""
+
+    template_name = "blog/blog_create.html.j2"
+    model = Blog
+    fields = ["name", "tagline"]
 
 
 class BlogDeleteView(DeleteView):
     """Displays a confirmation for the deletion of a blog"""
 
-    template_name = 'blog/blog_delete_confirm.html.j2'
+    template_name = "blog/blog_delete_confirm.html.j2"
     model = Blog
-    context_object_name = 'blog'
-    success_url = reverse_lazy('blog:blog_list')
+    context_object_name = "blog"
+    success_url = reverse_lazy("blog:blog_list")
 
 
 class EntryListView(ListView):
     """Displays all the entries in a blog in a paginated fashion"""
 
-    template_name = 'blog/entry_list.html.j2'
+    template_name = "blog/entry_list.html.j2"
     paginate_by = 5
     model = Entry
 
     def get_queryset(self):
-        return super().get_queryset().filter(blog=self.kwargs['pk'])
+        return super().get_queryset().filter(blog=self.kwargs["pk"])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['blog'] = Blog.objects.get(pk=self.kwargs['pk'])
+        context["blog"] = Blog.objects.get(pk=self.kwargs["pk"])
+        return context
+
+
+class EntryAuthorListView(ListView):
+    template_name = "blog/entry_list.html.j2"
+    paginate_by = 5
+    model = Entry
+
+    def get_queryset(self):
+        return super().get_queryset().filter(authors=self.kwargs["author_id"])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["author"] = Author.objects.get(pk=self.kwargs["author_id"])
         return context
 
 
 class EntryDetailView(DetailView):
     """Displays the details of a particular entry"""
 
-    template_name = 'blog/entry_details.html.j2'
-    context_object_name = 'entry'
+    template_name = "blog/entry_details.html.j2"
+    context_object_name = "entry"
     model = Entry
 
 
@@ -52,25 +74,55 @@ class EntryDeleteView(DeleteView):
     """Displays a confirmation for the deletion of an entry"""
 
     model = Entry
-    context_object_name = 'entry'
-    template_name = 'blog/entry_delete_confirm.html.j2'
+    context_object_name = "entry"
+    template_name = "blog/entry_delete_confirm.html.j2"
 
     def get_success_url(self) -> str:
-        return reverse('blog:entry_list', kwargs={'pk': self.object.blog_id})
+        return reverse("blog:entry_list", kwargs={"pk": self.object.blog_id})
+
+
+class EntryCreateView(CreateView):
+    """Form to create a new entry"""
+
+    model = Entry
+    template_name = "blog/entry_create.html.j2"
+    fields = [
+        "headline",
+        "body_text",
+        "pub_date",
+        "mod_date",
+        "number_of_comments",
+        "number_of_pingbacks",
+        "rating",
+        "authors",
+    ]
+
+    def form_valid(self, form):
+        form.instance.blog = Blog.objects.get(pk=self.kwargs["blog_id"])
+        return super().form_valid(form)
 
 
 class EntryUpdateView(UpdateView):
     """Update form to update a given entry"""
 
-    template_name = 'blog/entry_update_details.html.j2'
+    template_name = "blog/entry_update_details.html.j2"
     model = Entry
-    fields = ['blog', 'headline', 'body_text', 'pub_date', 'mod_date',
-              'number_of_comments', 'number_of_pingbacks', 'rating', 'authors']
+    fields = [
+        "blog",
+        "headline",
+        "body_text",
+        "pub_date",
+        "mod_date",
+        "number_of_comments",
+        "number_of_pingbacks",
+        "rating",
+        "authors",
+    ]
 
 
 class BlogUpdateView(UpdateView):
     """Update form to update a blog"""
 
-    template_name = 'blog/blog_update_details.html.j2'
+    template_name = "blog/blog_update_details.html.j2"
     model = Blog
-    fields = ['name', 'tagline']
+    fields = ["name", "tagline"]
